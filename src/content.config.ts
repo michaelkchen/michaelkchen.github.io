3b228cc79a07e2796525d355db4f3c85efdbb146
Michael Chen <38442638+electriql@users.noreply.github.com>
2026-05-19 21:16:51 -0700

    added dates and changed page formatting

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,6 +5,7 @@ import { z } from "astro/zod"
 const works = defineCollection({
     loader: glob({ base: 'src/content/works', pattern:'*.mdx' }),
     schema: z.object({
+        date: z.string(),
         priority: z.int(),
         thumbnail: z.string(),
         title: z.string(),
