1d0f95fefab6a6ddc87ed179f386793c82b0c561
Michael Chen <38442638+electriql@users.noreply.github.com>
2026-05-27 23:02:10 -0700

    cursor effect, note to change colorA to darker version of colorB

--- src/components/shaders.js
+++ src/components/shaders.js
@@ -10,17 +10,47 @@ const softwareColor = 'rgb(8, 27, 48)'
 const artColor = 'rgb(17, 8, 48)'
 var color = defaultColor;
 
+var components = [{
+    type: 'Swirl',
+    id: 'liquid',
+    props: {
+        colorA: 'rgb(1, 1, 1)',
+        colorB: color,
+        blend: 50,
+        detail: 1,
+        speed: 0.5,
+    }, 
+}]
+for (var i = 0; i < 6; i++) {
+    components.push({
+        type: 'Circle',
+        id: 'circle' + i,
+        props: {
+            blendMode: 'overlay',
+            radius: 0.25,
+            softness: 0.2,
+            visible: true,
+            // maskType: 'alphaInverted',
+            center: {
+                type: 'mouse-position'
+            }
+        },
+    })
+}
+
 const shader = await createShader(canvas, {
     colorSpace: 'srgb',
-    components: [
-        { type: 'Swirl', id: 'liquid', props: {
-            colorA: 'rgb(0, 0, 0)',
-            colorB: color,
-            blend: 50,
-            detail: 1,
-            speed: 0.5,
-        } },
-    ]
+    components: components
+        
+        // {
+        //     type: 'Glow',
+        //     id: 'glow',
+        //     props: {
+        //         intensity: 1000,
+        //         threshold: 0,
+        //         maskSource: 'circle'
+        //     },
+        // }
 })
 
 const observer = new IntersectionObserver((entries) => {
@@ -50,7 +80,6 @@ async function crossColor(newColor) {
         onUpdate: (latest) => {
             const rgb = latest.match(/\d+/g);
             color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
-            console.log(color)
             shader.update('liquid', {colorB: color})
         }
     })
